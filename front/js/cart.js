a50baff6b26d96c8da2b2c2ff73955652ca78d6a
NLN74 <ration753@gmail.com>
2022-12-03 17:45:25 +0100

    ajout du bouton supprimer dans le panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -4,7 +4,10 @@ let produitPanier = JSON.parse(localStorage.getItem("Canap"));
 
 
 //mise en place de l'HTML
+
+function insertionHtml (){
 for (let i = 0; i < produitPanier.length; i++) {
+
 
     const doc = document.getElementById('cart__items')
 
@@ -32,35 +35,34 @@ for (let i = 0; i < produitPanier.length; i++) {
                 </div>
             </article>`;
 
-    const quantity = document.getElementById('quantity');
 
+};
+};
 
-    quantity.addEventListener('change', function () {
+insertionHtml();
 
-
-
-    })
-
-}
-
+//Prix total + Nombre total d'article
 
 let prixTotalCalcul = [];
 let numberTotalOfProduit = [];
-for (let f = 0; f < produitPanier.length; f++) {
+
+function prixTotal_nombreArticle (){
+    for (let f = 0; f < produitPanier.length; f++) {
 
 
-    let priceProduit = produitPanier[f].objectPrice * produitPanier[f].objectNumber;
+        let priceProduit = produitPanier[f].objectPrice * produitPanier[f].objectNumber;
 
-    //Utilisation de parseInt pour retirer les string
-    let numberOfProduit = parseInt(produitPanier[f].objectNumber, 10);
+        //Utilisation de parseInt pour retirer les string
+        let numberOfProduit = parseInt(produitPanier[f].objectNumber, 10);
 
-    //Mettre les prix dans un tableau
-    prixTotalCalcul.push(priceProduit);
-    numberTotalOfProduit.push(numberOfProduit);
+        //Mettre les prix dans un tableau
+        prixTotalCalcul.push(priceProduit);
+        numberTotalOfProduit.push(numberOfProduit);
 
 
 
-}
+    };
+
 
 //Calcul pour le total des Canapé ainsi que du nombre de Canapé
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
@@ -68,13 +70,47 @@ const prixTotal = prixTotalCalcul.reduce(reducer);
 const numberTotal = numberTotalOfProduit.reduce(reducer);
 
 //Prix total des Canapé
-const totalPrice = document.getElementById('totalPrice')
+const totalPrice = document.getElementById('totalPrice');
 totalPrice.innerHTML = prixTotal;
 
 //Total du nombre de Canapé
-const totalQuantity = document.getElementById('totalQuantity')
+const totalQuantity = document.getElementById('totalQuantity');
 totalQuantity.innerHTML = numberTotal;
 
 
+};
+
+prixTotal_nombreArticle();
+
+
+
+
+//Supprimer l'élément au click
+
+function deleteProduct(){
+
+    let deleteItem = document.querySelectorAll('.deleteItem')
+
+    for (let j = 0; j < deleteItem.length; j++){
+        deleteItem[j].addEventListener("click", (event) =>{
+
+            event.preventDefault();
+
+            let idDelete = produitPanier[j].objectId;
+            let colorDelete = produitPanier[j].objectColors;
+
+            produitPanier = produitPanier.filter(el => el.objectId !== idDelete || el.objectColors !== colorDelete );
+ 
+            localStorage.setItem("Canap", JSON.stringify(produitPanier))
+
+            alert("Ce produit a bien été supprimé du panier");
+            location.reload();
+
+        })
+    }
+
+};
+
+deleteProduct();
 
 
